335437a961ac3ce113813f96953168db3883a855
konstantin_work <konstantin.ghevondyan@picsart.academy>
2026-01-21 10:51:44 +0400

    Implement Queue class with core methods and utility functions

--- Queue/queue.js
+++ Queue/queue.js
@@ -1,1 +1,44 @@
-class Queue {}
+class Queue {
+    array = [];
+    constructor(arr) {
+        this.array = arr || [];
+    }
+
+    enqueue = (el) => {
+        this.array.push(el);
+        return true;
+    }
+
+    dequeue = () => {
+        return this.array.shift();
+    }
+
+    peek = () => {
+        return this.array[0];
+    }
+
+    isEmpty = () => {
+        return this.array.length === 0;
+    }
+
+    size = () => {
+        return this.array.length;
+    }
+
+    clear = () => {
+        this.array = [];
+        return true;
+    }
+
+    // utility functions
+
+    toArray = () => {
+        return [...this.array];
+    }
+
+    toString = () => {
+        return String(this.array);
+    }
+}
+
+export default Queue;
