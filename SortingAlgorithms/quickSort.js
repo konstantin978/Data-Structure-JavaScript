6341ed5adc9da7db0d82eadd1fc750e8fef4b7f4
konstantin_work <konstantin.ghevondyan@picsart.academy>
2026-02-01 16:24:32 +0400

    Refactor QuickSort implementation for improved readability and functionality

--- SortingAlgorithms/quickSort.js
+++ SortingAlgorithms/quickSort.js
@@ -1,26 +1,33 @@
-const quickSort = (array, left = 0, right = array.length - 1) => {
-  if (left < right) {
-    const pivotIndex = partition(array, left, right);
-    quickSort(array, left, pivotIndex - 1);
-    quickSort(array, pivotIndex + 1, right);
-  }
-  return array;
-};
+const QuickSort = (inputArray) => {
+  if (!Array.isArray(inputArray)) return [];
+  const arr = [...inputArray];
 
-const partition = (array, left, right) => {
-  const mid = Math.floor((left + right) / 2);
-  [array[mid], array[right]] = [array[right], array[mid]];
+  const partition = (left, right) => {
+    const mid = Math.floor((left + right) / 2);
+    [arr[mid], arr[right]] = [arr[right], arr[mid]];
 
-  const pivot = array[right];
-  let i = left - 1;
+    const pivot = arr[right];
+    let i = left - 1;
 
-  for (let j = left; j < right; j++) {
-    if (array[j] < pivot) {
-      i++;
-      [array[i], array[j]] = [array[j], array[i]];
+    for (let j = left; j < right; j++) {
+      if (arr[j] < pivot) {
+        i++;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
     }
-  }
 
-  [array[i + 1], array[right]] = [array[right], array[i + 1]];
-  return i + 1;
+    [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
+    return i + 1;
+  };
+
+  const sort = (left, right) => {
+    if (left < right) {
+      const pivotIndex = partition(left, right);
+      sort(left, pivotIndex - 1);
+      sort(pivotIndex + 1, right);
+    }
+  };
+
+  sort(0, arr.length - 1);
+  return arr;
 };
